ebdcbfaa095d32c74c6a81e434c8fb438f0e4381
kespinak <kespinak@gmail.com>
2023-01-24 16:42:54 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+//? HOW IN THE HELL DOES ALL THE TYPING WORK EXCEPT FOR TYPING DELETE BUTTON
+//? ALSO, WHY DOES CLICKING WORK FOR LETTERS BUT NOT THE BACKSPACE OR ENTER???
+//? 41
+
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import React, { createContext, useState } from 'react';
+import { boardDefault } from './components/Words';
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currentAttempt, setCurrentAttempt] = useState({attempt: 0, letterPos: 0});
+
+  const onSelectLetter = (keyVal) => {
+    if (currentAttempt.letterPos > 4) {return};
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos + 1})
+  };
+
+  const onDelete = () => {
+    if (currentAttempt.letterPos === 0) {return}
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = '';
+    setBoard(newBoard);
+    setCurrentAttempt( {...currentAttempt, letterPos: currentAttempt.letterPos - 1});
+  }
+
+  const onEnter = () => {
+    if (currentAttempt.letterPos !== 5) {return}
+      setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0});
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider 
+        value={{
+          board, 
+          setBoard, 
+          currentAttempt, 
+          setCurrentAttempt, 
+          onSelectLetter, 
+          onDelete, 
+          onEnter
+        }} 
+      >
+        <div className='game'>
+          <Board /> 
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/components/Key.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react'
+import { AppContext } from '../App'
+
+function Key({keyVal, bigKey}) {
+  const { onSelectLetter, OnDelete, OnEnter} = useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyVal === 'ENTER') {
+      OnEnter()
+    } else if (keyVal === 'DELETE') {
+      OnDelete()
+    } else {
+      onSelectLetter(keyVal)
+    }
+  }; 
+
+  return (
+    <div className='key' id={bigKey && 'big'} onClick={selectLetter}>
+      {keyVal}
+    </div>
+  )
+}
+
+export default Key;
